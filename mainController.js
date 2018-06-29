66ce18127bbb5145020a4b7c65c459274cb02c7e
saksham <saksham10.saxena@gmail.com>
2018-06-29 19:48:14 +0530

    added checkboxes based on checked status

--- mainController.js
+++ mainController.js
@@ -1,3 +1,10 @@
-angular.module('myApp').controller('mainController', function($scope){
+angular.module('myApp').controller('mainController', function ($scope) {
+
+
+
+    $scope.taskArr = [
+        { checkedStatus: true, text: 'i am a task' },
+        { checkedStatus: false, text: '2 i am also a task' }
+    ];
 
 });
