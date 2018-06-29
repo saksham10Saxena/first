0164a15a9ec9a9a5f508a20f252ea198f927bf08
saksham <saksham10.saxena@gmail.com>
2018-06-29 20:22:04 +0530

    changed checkedStatus on clicking checkbox

--- mainController.js
+++ mainController.js
@@ -7,4 +7,22 @@ angular.module('myApp').controller('mainController', function ($scope) {
         { checkedStatus: false, text: '2 i am also a task' }
     ];
 
+    /**
+     * 
+     * @param {*} taskIndex index of task in the list
+     * @param {*} currentCheckedStatus  checked status of the clicked checkbox
+     * @desc changes the cuurrentCheckedStatus of the selected checkbox
+     */
+    $scope.handleCheckboxClick = function(taskIndex, currentCheckedStatus){
+        console.log(taskIndex , currentCheckedStatus);
+        if(taskIndex === undefined){
+            throw 'task index is invalid';
+        }
+        if(currentCheckedStatus === undefined){
+            throw 'currentCheckedStatus is invalid';
+        }
+        $scope.taskArr[taskIndex].checkedStatus = !currentCheckedStatus;
+    }
+
+
 });
