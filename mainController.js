c08c4673aa3f3ee0cf658151c7bd2233d8aed297
saksham <saksham10.saxena@gmail.com>
2018-07-27 22:08:40 +0530

    Tried to create groupArr

--- mainController.js
+++ mainController.js
@@ -1,11 +1,27 @@
 angular.module('myApp').controller('mainController', function ($scope) {
 
 
+    
+    $scope.groupArr = [
+        { name: Group-4, taskArr:[{ checkedStatus: true, text: 'i am a task' }, {checkedStatus: false, text: 'i am also a task' }]},
+        { name: Group-5, taskArr:[{ checkedStatus: true, text: 'i am a task' }, {checkedStatus: false, text: 'i am also a task' }]}
 
-    $scope.taskArr = [
-        { checkedStatus: true, text: 'i am a task' },
-        { checkedStatus: false, text: '2 i am also a task' }
+        
     ];
+  
+    $scope.visibleTaskArr = [
+        { checkedStatus: true, text: 'i am a task' },
+        { checkedStatus: false, text: 'i am also a task' }
+    ];
+    $scope.groupAdd = function(){
+        $scope.groupArr.push({name=$scope.groupnameInput});
+        $scope.groupnameInput = "";
+    };
+    $scope.selectedGroup(selectedGroupIndex) = function() {
+        visibleTaskArr = selectedGroup.TaskArr;
+    }
+
+    
 
     /**
      * 
@@ -22,6 +38,25 @@ angular.module('myApp').controller('mainController', function ($scope) {
         }
         $scope.taskArr[taskIndex].checkedStatus = !currentCheckedStatus;
     }
-
+    $scope.toAdd = function(){
+        $scope.taskArr.push({text: $scope.todoInput, checkedStatus:$scope.handleCheckboxClick});
+        $scope.todoInput = "";
+    };
+ 
+    $scope.remove = function() {
+        var oldList = $scope.taskArr;
+        $scope.taskArr = [ ];
+        angular.forEach(oldList, function(x) {
+            if (!x.checkedStatus) $scope.taskArr.push(x);
+        });
+    };
+    function openGroup(groupNumber) {
+        var i;
+        var x = document.getElementsByClassName("group");
+        for (i = 0; i < x.length; i++) {
+           x[i].style.display = "none";  
+        }
+        document.getElementById(groupNumber).style.display = "block";  
+    }
 
 });
